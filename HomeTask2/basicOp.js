881bf43bf849c30714d63a74af451592e6d8f146
Sairam Bomma <Sairam_Bomma@epam.com>
2021-01-19 10:55:23 +0530

    [HomeTask2] | made changes as per pull request- shortened functions, added zero check for division operator

--- HomeTask2/basicOp.js
+++ HomeTask2/basicOp.js
@@ -10,18 +10,16 @@
 
 function basicOp(operation, value1, value2) {
     const operators = {
-        '+': (number1, number2) => {
-            return number1 + number2;
-        },
-        '-': (number1, number2) => {
-            return number1 - number2;
-        },
+        '+': (number1, number2) => number1 + number2,
+        '-': (number1, number2) => number1 - number2,
+        '*': (number1, number2) => number1 * number2,
         '/': (number1, number2) => {
+            if (Number(number2) === 0) {
+                throw new Error("number2 can't be zero");
+            }
             return number1 / number2;
         },
-        '*': (number1, number2) => {
-            return number1 * number2;
-        }
+
     }
 
     return operators[operation](value1, value2);
@@ -31,3 +29,15 @@ console.log(basicOp('+', 4, 7)); //expected 11
 console.log(basicOp('-', 15, 18)); //expected -3
 console.log(basicOp('*', 5, 5)); //expected 25
 console.log(basicOp('/', 49, 7)); //expected 7
+function test() {
+    try {
+        let result = basicOp('/', 49, 0); //expected to throw error
+        console.log(result);
+    }
+    catch (e) {
+        console.error(e.message);
+    }
+
+}
+
+test();//execution test expecting to console error message
